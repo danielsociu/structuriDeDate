8c9093e1dc76cc8b6c062dc77960c1832dce4e40
Daniel Sociu <danielsociu00@gmail.com>
2020-04-29 21:59:23 +0300

    Done.

--- treapuri.cpp
+++ treapuri.cpp
@@ -50,6 +50,10 @@ public:
         rad = new T(key,priority,left,right);
     }
 
+    T* getRoot(){
+        return this->rad;
+    }
+
     bool search(int key){
         T* it=rad;
         while(it->key != key && it != nil)
@@ -112,36 +116,39 @@ public:
         //we need recursiveness to balance?? No pointer towards father, I guess I could've done a balance(T* &node, int key, int priority) that would balance recursivly
         //but no reason for extra work when we can do it all with recursiveness
     }
-    void erase(T* &node,int key){
+    void erase(T* &node,int key,int& ok){
         if( node == nil)
             return;
 
         if(node->key > key)
-            erase(node->left, key);
+            erase(node->left, key,ok);
         else if(node->key < key)
-            erase(node->right, key);
+            erase(node->right, key,ok);
         else{
             if(node->left == nil && node->right==nil)
             {
                 delete node;
                 node = nil;
+                ok=1;
             }
             else{
                 if(node->left->priority > node->right->priority)
                 {
                     this->rotateLeft(node);
-                    erase(node->right ,key);
+                    erase(node->right ,key,ok);
                 }
                 else
                 {
                     this->rotateRight(node);
-                    erase(node->left,key);
+                    erase(node->left,key,ok);
                 }
             }
         }
     }
-    void erase(int key){
-        erase(this->rad,key);
+    int erase(int key){
+        int ok=0;
+        erase(this->rad,key,ok);
+        return ok;
     }
     //after a split the main treap will be empty;
     friend void split(Treap& trp, Treap& trpleft, Treap& trpright,int key){
@@ -210,11 +217,68 @@ public:
             it=it->left;
         return it->key;
     }
+    int successor(int key){
+        int sol=-1;
+        if(key>this->getMax()){
+            return -1;
+        }
+        T* it = this->rad;
+        while(it->key != key && it!=nil){
+            if(it-> key > key){
+                sol = it->key;
+                it = it->left;
+            }else
+                it = it->right;
+        }
+        if(it->key == key)
+            return key;
+        if(it!=nil && it->right != nil)
+        {
+            it=it->right;
+            while(it->left!=nil)
+                it = it->left;
+            sol=it->key;
+        }
+        return sol;
+    }
+    int predecessor(int key){
+        int sol=-1;
+        if(key < this->getMin()){
+            return -1;
+        }
+        T* it = this->rad;
+        while(it->key != key && it!=nil){
+            if(it->key < key){
+                sol=it->key;
+                it = it->right;
+            }
+            else
+                it = it->left;
+        }
+        if(it->key == key)
+            return key;
+        if(it!= nil && it->left != nil)
+        {
+            it=it->left;
+            while(it->right != nil)
+                it=it->right;
+            sol=it->key;
+        }
+        return sol;
+
+    }
     void inordine(ostream& out,T* node){
         if (node == nil) return;
         inordine(out,node->left);
         out<<left<<setw(10)<<node->key<<' '<<node->priority<<'\n';
         inordine(out,node->right);
+    }
+    void inordineLimited(T* node,int X,int Y){
+        if (node == nil) return;
+        inordineLimited(node->left,X,Y);
+        if(node->key <=Y && node->key>=X)
+            cout<<node->key<<' ';
+        inordineLimited(node->right,X,Y);
     }
     Treap& operator=(const Treap& cpy){
         if(this!= &cpy){
@@ -248,37 +312,53 @@ int main()
 {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
-    Treap myTreap(5,10);
-    //cout<<myTreap<<'\n';
-    myTreap.insert(20,15);
-    myTreap.insert(10,45);
-    myTreap.insert(50,40);
-    myTreap.insert(7,30);
-    myTreap.insert(2,1);
-    //cin>>myTreap;
-    //cout<<myTreap;
-    Treap lft,rght;
-    split(myTreap,lft,rght,10);
-    cout<<'\n';
-    cout<<lft;
-    cout<<'\n';
-    cout<<rght;
-    cout<<'\n';
-    join(myTreap,lft,rght,10);
-    cout<<myTreap;
-    cout<<'\n';
-    cout<<lft;
-    cout<<'\n';
-    cout<<rght;
-    cout<<'\n';
-    myTreap.insert(35,10);
-    cout<<myTreap;
-    cout<<'\n';
-    myTreap.erase(35);
-    cout<<myTreap;
-    cout<<'\n';
-    cout<<myTreap.getKthKey(3)<<'\n';
-    cout<<myTreap.getMax()<<'\n';
-    cout<<myTreap.getMin()<<'\n';
-
+    freopen("abce.in","r",stdin);
+    freopen("abce.out","w",stdout);
+    int tip,x,y,cnt,ok,n;
+    Treap myTreap;
+    cin>>n;
+    //80 de punce pe infoarena
+    //Daca as fi avut acces la teste, as fi putut da debug
+    //Dar din pacate sunt private :(
+    FOR(i,1,n)
+    {
+        cin>>tip;
+        cin>>x;
+        switch(tip){
+            case 1:
+                myTreap.insert(x,rand()+1);
+                break;
+            case 2:
+                cnt=0;
+                ok=myTreap.erase(x);
+                /*while(1){
+                    ok=myTreap.erase(x);
+                    cnt++;
+                    if(!ok)
+                        break;
+                }*/
+                //cout<<cnt<<'\n';
+                break;
+            case 3:
+                if(myTreap.search(x))
+                    cout<<1;
+                else
+                    cout<<0;
+                cout<<'\n';
+                break;
+            case 4:
+                cout<<myTreap.predecessor(x)<<'\n';
+                break;
+            case 5:
+                cout<<myTreap.successor(x)<<'\n';
+                break;
+            case 6:
+                cin>>y;
+                myTreap.inordineLimited(myTreap.getRoot(),x,y);
+                cout<<'\n';
+                break;
+            default:
+                return 0;
+        }
+    }
 }
